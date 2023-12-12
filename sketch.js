65bf989223611d1fee5cfc51eae8270ca825b8c0
owenreilly <152304945+owenreilly@users.noreply.github.com>
2023-12-12 16:11:40 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,4 @@
-slapSound = loadSound('slap.mp3');
-tickleSound = loadSound('tickle.mp3');
-
 let stickFigure;
-let slapSound, tickleSound;
-
-function preload() {
-  slapSound = loadSound('slap.mp3'); // Replace 'slap.mp3' with your slap sound file
-  tickleSound = loadSound('tickle.mp3'); // Replace 'tickle.mp3' with your tickle sound file
-}
 
 function setup() {
   createCanvas(400, 400);
@@ -55,11 +46,81 @@ class StickFigure {
 
   slap() {
     // Slap action
-    slapSound.play();
+    // You can add any custom action here if needed
   }
 
   tickle() {
     // Tickle action
-    tickleSound.play();
+    // You can add any custom action here if needed
   }
 }
+
+
+
+
+
+// slapSound = loadSound('slap.mp3');
+// tickleSound = loadSound('tickle.mp3');
+
+// let stickFigure;
+// let slapSound, tickleSound;
+
+// function preload() {
+//   slapSound = loadSound('slap.mp3'); // Replace 'slap.mp3' with your slap sound file
+//   tickleSound = loadSound('tickle.mp3'); // Replace 'tickle.mp3' with your tickle sound file
+// }
+
+// function setup() {
+//   createCanvas(400, 400);
+//   stickFigure = new StickFigure(width / 2, height / 2);
+// }
+
+// function draw() {
+//   background(220);
+
+//   stickFigure.display();
+
+//   // Display bars for slap and tickle
+//   fill(50, 200, 50);
+//   rect(width - 40, 0, 40, height); // Tickle bar
+//   fill(200, 50, 50);
+//   rect(0, 0, 40, height); // Slap bar
+// }
+
+// function mousePressed() {
+//   if (mouseX > width - 40) {
+//     // Tickle
+//     stickFigure.tickle();
+//   } else if (mouseX < 40) {
+//     // Slap
+//     stickFigure.slap();
+//   }
+// }
+
+// class StickFigure {
+//   constructor(x, y) {
+//     this.x = x;
+//     this.y = y;
+//   }
+
+//   display() {
+//     // Draw stick figure
+//     fill(0);
+//     ellipse(this.x, this.y - 30, 20, 20); // Head
+//     line(this.x, this.y, this.x, this.y + 50); // Body
+//     line(this.x, this.y + 20, this.x - 20, this.y); // Left arm
+//     line(this.x, this.y + 20, this.x + 20, this.y); // Right arm
+//     line(this.x, this.y + 50, this.x - 20, this.y + 80); // Left leg
+//     line(this.x, this.y + 50, this.x + 20, this.y + 80); // Right leg
+//   }
+
+//   slap() {
+//     // Slap action
+//     slapSound.play();
+//   }
+
+//   tickle() {
+//     // Tickle action
+//     tickleSound.play();
+//   }
+// }
